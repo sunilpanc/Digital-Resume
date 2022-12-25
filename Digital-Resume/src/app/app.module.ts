3dc2529f66e655fb02ad630ec5adc4cfeba3ddad
sunilpanch <sunilpanchal123.sp@gmail.com>
2022-12-25 21:24:35 +0530

    home page completed with some component added

--- Digital-Resume/src/app/app.module.ts
+++ Digital-Resume/src/app/app.module.ts
@@ -6,12 +6,18 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { ResumeComponent } from './resume/resume.component';
+import { ContactmeComponent } from './contactme/contactme.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    ResumeComponent,
+    ContactmeComponent
   ],
   imports: [
     BrowserModule,
